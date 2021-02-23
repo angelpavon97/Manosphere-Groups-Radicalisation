df059c00fff2df6409d3a84f84d9a1b52bb00be0
Ángel <angelpavonperez@gmail.com>
2021-02-23 11:15:39 +0100

    Conector con la base de datos y extracción de los sitios web  más compartidos

---
+++ IncelsSQL.py
@@ -0,0 +1,127 @@
+import mysql.connector
+from config import config # Dictionary with mysql config
+import tldextract
+import re
+
+class IncelsSQL:
+    def __init__(self):
+        self.cnx = mysql.connector.connect(**config)
+
+    def __get_url_regex(self): # https://stackoverflow.com/questions/9760588/how-do-you-extract-a-url-from-a-string-using-python
+        regex = r'('
+        # Scheme (HTTP, HTTPS, FTP and SFTP):
+        regex += r'(?:(https?|s?ftp):\/\/)?'
+        # www:
+        regex += r'(?:www\.)?'
+        regex += r'('
+        # Host and domain (including ccSLD):
+        regex += r'(?:(?:[A-Z0-9][A-Z0-9-]{0,61}[A-Z0-9]\.)+)'
+        # TLD:
+        regex += r'([A-Z]{2,6})'
+        # IP Address:
+        regex += r'|(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+        regex += r')'
+        # Port:
+        regex += r'(?::(\d{1,5}))?'
+        # Query path:
+        regex += r'(?:(\/\S+)*)'
+        regex += r')'
+
+        return regex
+
+    def close_connection(self):
+        self.cnx.close()
+
+    def get_comments(self):
+        cursor = self.cnx.cursor()
+
+        query = ("SELECT body FROM comments")
+
+        cursor.execute(query)
+
+        comments = []
+
+        for body in cursor:
+            comments.append(body[0].decode('UTF-8'))
+
+        return comments
+
+    def get_urls_statistics(self):
+        cursor = self.cnx.cursor()
+
+        query = ("SELECT url FROM url_statistics")
+
+        cursor.execute(query)
+
+        urls = []
+
+        for url in cursor:
+            urls.append(url[0])
+
+        return urls
+
+    def get_url_root(self):
+        cursor = self.cnx.cursor()
+
+        query = ("SELECT url FROM url_statistics")
+
+        cursor.execute(query)
+
+        urls = {}
+
+        for url in cursor:
+            url = url[0]
+            ext = tldextract.extract(url)
+            
+            if ext.subdomain != '' and ext.subdomain != 'www':
+                domain = ext.subdomain + '.' + ext.domain
+            else:
+                domain = ext.domain
+
+            if domain in urls:
+                urls[domain] += 1
+            else:
+                urls[domain] = 1
+
+        return dict(sorted(urls.items(), key=lambda item: item[1], reverse=True))
+
+
+    def get_urls_from_comments(self):
+        cursor = self.cnx.cursor()
+        query = ("SELECT body FROM comments")
+        cursor.execute(query)
+
+        urls = []
+
+        regex = self.__get_url_regex()
+        find_urls = re.compile(regex, re.IGNORECASE)
+
+        for body in cursor:
+            comment = body[0].decode('UTF-8')
+            
+            found_urls = find_urls.findall(comment)
+
+            for url in found_urls:
+                urls.append(url[0])
+
+        return urls
+
+    def get_unique_urls_from_comments(self, urls):
+
+        unique_urls = {}
+
+        for url in urls:
+            ext = tldextract.extract(url)
+
+            if ext.subdomain != '' and ext.subdomain != 'www':
+                domain = ext.subdomain + '.' + ext.domain
+            else:
+                domain = ext.domain
+
+            if domain in unique_urls:
+                unique_urls[domain] += 1
+            else:
+                unique_urls[domain] = 1
+
+        return dict(sorted(unique_urls.items(), key=lambda item: item[1], reverse=True))
+            
---
+++ main.py
@@ -0,0 +1,39 @@
+from IncelsSQL import IncelsSQL
+from random import randint
+import matplotlib.pyplot as plt
+
+def reduce_dictionary(dictionary, min = 200, max = 400):
+    return {k:dictionary[k] for k in dictionary if dictionary[k] > min and dictionary[k] <= max}
+
+def plot_dictionary(dictionary, min, max):
+    d = reduce_dictionary(dictionary, min, max)
+
+    plt.bar(range(len(d)), list(d.values()), align='center', color = 'orange')
+    plt.xticks(range(len(d)), list(d.keys()), rotation='vertical')
+    plt.show()
+
+connection = IncelsSQL()
+
+# comments = connection.get_comments()
+# urls = connection.get_urls_statistics()
+
+# print('Number of comments: ', len(comments))
+# print(comments[randint(0,len(comments))])
+
+# print('Number of urls: ', len(urls))
+# print(urls[randint(0,len(urls))])
+
+# urls = connection.get_url_root()
+# print(urls)
+# plot_dictionary(urls)
+
+urls = connection.get_urls_from_comments()
+print('Number of urls: ', len(urls))
+print(urls[randint(0,len(urls))])
+
+unique_urls = connection.get_unique_urls_from_comments(urls)
+print(unique_urls)
+plot_dictionary(unique_urls, 200, 400)
+plot_dictionary(unique_urls, 100, 200)
+
+connection.close_connection()
